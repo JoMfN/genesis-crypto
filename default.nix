150b7e54d5fe2cb57b5603eb9ec77642a427b49b
ZENODE <zenodeapp@gmail.com>
2026-05-07 14:00:05 +0200

    Revert: nix files from cronos-v1.1.1

--- default.nix
+++ default.nix
@@ -1,27 +1,19 @@
-{
-  lib,
-  stdenv,
-  buildGoApplication,
-  nix-gitignore,
-  coverage ? false, # https://tip.golang.org/doc/go1.20#cover
-  rocksdb,
-  network ? "mainnet", # mainnet|testnet
-  rev ? "dirty",
-  static ? stdenv.hostPlatform.isStatic,
-  nativeByteOrder ? true, # nativeByteOrder mode will panic on big endian machines
+{ lib
+, stdenv
+, buildGoApplication
+, nix-gitignore
+, buildPackages
+, coverage ? false # https://tip.golang.org/doc/go1.20#cover
+, rocksdb
+, network ? "mainnet"  # mainnet|testnet
+, rev ? "dirty"
+, static ? stdenv.hostPlatform.isStatic
+, nativeByteOrder ? true # nativeByteOrder mode will panic on big endian machines
 }:
 let
   version = "v1.1.1";
   pname = "genesisd";
-  tags = [
-    "ledger"
-    "netgo"
-    network
-    "rocksdb"
-    "grocksdb_no_link"
-    "pebbledb"
-    "objstore"
-  ] ++ lib.optionals nativeByteOrder [ "nativebyteorder" ];
+  tags = [ "ledger" "netgo" network "rocksdb" "grocksdb_no_link" ] ++ lib.optionals nativeByteOrder [ "nativebyteorder" ];
   ldflags = lib.concatStringsSep "\n" ([
     "-X github.com/cosmos/cosmos-sdk/version.Name=genesis"
     "-X github.com/cosmos/cosmos-sdk/version.AppName=${pname}"
@@ -32,48 +24,30 @@ let
   buildInputs = [ rocksdb ];
 in
 buildGoApplication rec {
-  inherit
-    pname
-    version
-    buildInputs
-    tags
-    ldflags
-    ;
-  src = (
-    nix-gitignore.gitignoreSourcePure [
-      "/*" # ignore all, then add whitelists
-      "!/x/"
-      "!/app/"
-      "!/cmd/"
-      "!/client/"
-      "!/versiondb/"
-      "!/memiavl/"
-      "!/store/"
-      "!go.mod"
-      "!go.sum"
-      "!gomod2nix.toml"
-    ] ./.
-  );
+  inherit pname version buildInputs tags ldflags;
+  src = (nix-gitignore.gitignoreSourcePure [
+    "/*" # ignore all, then add whitelists
+    "!/x/"
+    "!/app/"
+    "!/cmd/"
+    "!/client/"
+    "!/versiondb/"
+    "!/memiavl/"
+    "!/store/"
+    "!go.mod"
+    "!go.sum"
+    "!gomod2nix.toml"
+  ] ./.);
   modules = ./gomod2nix.toml;
   pwd = src; # needed to support replace
-  subPackages = [ "cmd/cronosd" ];
+  subPackages = [ "cmd/genesisd" ];
   buildFlags = lib.optionalString coverage "-cover";
   CGO_ENABLED = "1";
   CGO_LDFLAGS = lib.optionalString (rocksdb != null) (
-    if static then
-      "-lrocksdb -pthread -lstdc++ -ldl -lzstd -lsnappy -llz4 -lbz2 -lz"
-    else if stdenv.hostPlatform.isWindows then
-      "-lrocksdb-shared"
-    else
-      "-lrocksdb -pthread -lstdc++ -ldl"
+    if static then "-lrocksdb -pthread -lstdc++ -ldl -lzstd -lsnappy -llz4 -lbz2 -lz"
+    else if stdenv.hostPlatform.isWindows then "-lrocksdb-shared"
+    else "-lrocksdb -pthread -lstdc++ -ldl"
   );
-
-  postInstall = ''
-    # buildGoApplication installa bin/cro nosd; rinominalo a genesisd
-    if [ -f "$out/bin/cronosd" ]; then
-      mv "$out/bin/cronosd" "$out/bin/genesisd"
-    fi
-  '';
 
   postFixup = lib.optionalString (stdenv.isDarwin && rocksdb != null) ''
     ${stdenv.cc.bintools.targetPrefix}install_name_tool -change "@rpath/librocksdb.8.dylib" "${rocksdb}/lib/librocksdb.dylib" $out/bin/genesisd
